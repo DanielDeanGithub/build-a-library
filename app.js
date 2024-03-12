fe09554f877db5bb5378abe55ba6c4a1a4b1a980
DanielDeanGithub <github-new@inboxrefresh.com>
2024-03-12 09:45:52 +0000

    Call .addRating() three times on historyOfEverything

--- app.js
+++ app.js
@@ -71,6 +71,9 @@ class Movie extends Media {
 const historyOfEverything = new Book('Bill Bryson', 'A Short History of Nearly Everything', 544);
 historyOfEverything.toggleCheckOutStatus();
 console.log(historyOfEverything.isCheckedOut);
+historyOfEverything.addRating(4);
+historyOfEverything.addRating(5);
+historyOfEverything.addRating(5);
 
 /*
 const test = new Media('test');
